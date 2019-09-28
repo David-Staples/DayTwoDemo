9455babb8c1cd4e2b940c88f8172f905a8b97f10
David Staples <Work@Lucass-MacBook-Pro.local>
2019-09-27 20:07:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/daytwodemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.daytwodemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.Unit as Unit1
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fun onClick(view : View) = Unit1
+    }
+}
